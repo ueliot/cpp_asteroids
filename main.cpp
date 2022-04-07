9b7fdc077c80a00de193f335e86ca51ef0673e97
Eliot Uriarte <euriarte@zumbach.es>
2022-04-07 14:23:18 +0200

    nave3 aceleracion

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 
-// https://www.youtube.com/watch?v=HCWghQtxlos&list=PLDD6B727E5B6B5E33
+// https://www.youtube.com/watch?v=B4LDrD-UedE&list=PLDD6B727E5B6B5E33&index=3
+// Suma de vectores para conseguir el vector resultante en la posicion de la direccion  de velocidad
+//aceleracion con tecla arriba
+//debe haber un cambio de posicion permanente  que es el de la velocidad
+//la velocidad se conserva
+//y cuando damos a la flecha de arriba a la velocidad se le suma un vector 
+//luego de esto se rota vector  y se suma luego el vector rotado con el anterior.
+
 #include "miniwin.h"
 using namespace miniwin;
-//#define _USE_MATH_DEFINES
 #include <cmath>
 using namespace std;
 
@@ -20,7 +26,7 @@ void rotar(float& x,float& y, float cx, float cy, float da){
    y= cy+r*sin(a);
 
 }
-//pinta un rectagulo centrada en x e y con una rotacion del angulo a)
+//pinta un rectagulo centrada en x e y con una rotacion del angulo a
 void pinta_nave(float x, float y, float a){
    float x1 = x, y1 = y - 40;
    float x2 = x - 15, y2 = y + 10;
@@ -37,18 +43,35 @@ void pinta_nave(float x, float y, float a){
 }
 
 int main() {
-   vredimensiona(500, 500);
-   float a=0;
-   for (float y = 500.0; y > -50.0; y-=2.0)
-   {
-      /* code */
+   vredimensiona(500, 500);   
+   float x=250, y=250, a=0;  //posicion de la nave
+   float vx = 0.5, vy=0.0; //velocidad inicial
+   int t= tecla();
+   while (t != ESCAPE){
+      x+= vx;   //desplazamiento horizontal 20 pixeles x seg
+      y+=vy;   //desplazamiento vertical  30 pixeles x seg (nave va para abajo)
+
+      if(t==DERECHA){
+         a-=8;   //restamos 5 grados si se presiona derecha
+      } else if (t==IZQUIERDA){
+         a+=8;
+      } else if (t==ARRIBA) {
+
+         float ax=0.0, ay= -0.5;
+         rotar(ax,ay,0.0,0.0,a);
+         vx=vx+ax;
+         vy=vy+ay;
+      }
+      //refrescar la pantall      
       borra();
-      pinta_nave(250,y,a);
-      //printf("El valor y: %5.2f.  y deal angulo: %5.2f. \n", y,a);
-      a+=2.0;
+      pinta_nave(x,y,a);   //refresca la nave 100 veces x seg
       refresca();
-      espera(5);
-   } 
+      espera(10); //solo pedimos 100 teclas x segundo  (esperamos 10 mms)
+      t=tecla();
+   }
+   vcierra();
+
+   
    
    return 0;
 }
